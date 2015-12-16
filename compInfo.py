c1516f7bbbde1c8c011bc025519342e18771dfaf
Juan David <juandominguez2017@u.northwestern.edu>
2015-12-16 13:12:23 -0500

    Making personal paths easier to manage

---
+++ compInfo.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+originalDataDirectory = 'put_your_personal_path_here'
+dataPoints500File = originalDataDirectory + 'put_your_path_here'
+outputDataDirectory = '../iProcessedData/'
