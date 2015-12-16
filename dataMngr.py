681fcd0f7a4fd76e4ffe91e89cb4dde7d17f9d50
Juan David <juandominguez2017@u.northwestern.edu>
2015-12-15 21:41:10 -0500

    Adding dataframe manipulation

--- dataMngr.py
+++ dataMngr.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import numpy as np
 from dataInfo import columnInfo
 
-def processDataframeForNP(df):
+def processDiscDataframe(df):    
+    from sklearn.feature_extraction import DictVectorizer as DV
+    vectorizer = DV( sparse = False )
+    df = df.to_dict('records')
+    numpyData = vectorizer.fit_transform(df)
+    return numpyData
+
+def processDataframeForNP(df, train=True):
     df = df.drop(columnInfo['extraneous'], axis=1) #Unecessary Info
-    pass
+    
+    outputData = None
+    if train:
+        outputData = df[columnInfo['target']]
+        outputData = outputData.as_matrix()
+    df.drop(columnInfo['target'])
+    
+    inputDiscrete = df[columnInfo['discrete']]
+    inputContinuous = df[columnInfo['continous']]
+    
+    inputDiscrete = inputDiscrete.as_matrix()
+    inputContinuous = processDiscDataframe(inputContinuous)
+    
+    inputData = np.concatenate((inputDiscrete, inputContinuous), axis=1)
+    return inputData, outputData
 
 def file2Dataframe(fileName):
     df = pd.read_csv(fileName, engine='c')
