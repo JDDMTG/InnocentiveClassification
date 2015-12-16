a17360fd61182cd1fe77279b7c46e96853ed9ba2
Juan David <juandominguez2017@u.northwestern.edu>
2015-12-15 22:25:33 -0500

    Adds preprocessing

--- dataMngr.py
+++ dataMngr.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
+import cPickle as pickle
+from os import path
 from dataInfo import columnInfo
 
 def processDiscDataframe(df):    
@@ -17,30 +19,37 @@ def processDataframeForNP(df, train=True):
     if train:
         outputData = df[columnInfo['target']]
         outputData = outputData.as_matrix()
-    df.drop(columnInfo['target'])
+    df.drop(columnInfo['target'], axis=1)
     
     inputDiscrete = df[columnInfo['discrete']]
     inputContinuous = df[columnInfo['continous']]
     
-    inputDiscrete = inputDiscrete.as_matrix()
-    inputContinuous = processDiscDataframe(inputContinuous)
+    inputDiscrete = processDiscDataframe(inputDiscrete)
+    inputContinuous = inputContinuous.as_matrix()
     
-    inputData = np.concatenate((inputDiscrete, inputContinuous), axis=1)
+    inputData = np.concatenate(( inputContinuous, inputDiscrete ), axis=1)
     return inputData, outputData
 
 def file2Dataframe(fileName):
     df = pd.read_csv(fileName, engine='c')
-    df = processDataframeForNP(df)
     return df
+
+def save(fileName, obj):
+    pickle.dump( obj, open( fileName, "wb" ) )
+    pass
+
+def load(fileName):
+    pickle.load( open( fileName, "rb" ) )
+    pass
+
+def makeFileName(inFileName, train=True):
+    base=path.basename(inFileName)
+    fileName, ext = path.splitext(base)
     
-def dataframe2Array(df):
-    pass
-
-def saveDataframeAsCSV(df):
-    pass
-
-def saveDataframeAsArray(df):
-    pass
-
-def seperateTestInputOutput(df):
-    pass
+    outputFileName = None
+    if train:
+        outputFileName = fileName + '_output.np'
+    
+    inputFileName = fileName + '_input.np'
+    
+    return inputFileName, outputFileName
