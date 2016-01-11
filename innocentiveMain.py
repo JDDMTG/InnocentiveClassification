62d68491c1ec0367e6ad0ddd533ce663d0558047
Juan David <juandominguez2017@u.northwestern.edu>
2016-01-10 23:10:36 -0600

    Prepares to generate two new models

--- innocentiveMain.py
+++ innocentiveMain.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 import dataMngr as dm
-import compInfo
+import compInfo as ci
 import machineLearning as ML
+from modelsInfo import models
+#ML.generateTestOutput(dm.load('RF.np'), dm.load(ci.outputDataDirectory + ci.testingInputPathFixed), "OutputData.csv")
 
-ML.generateTestOutput(dm.load('RF.np'), dm.load(ci.outputDataDirectory + ci.testingInputPathFixed), "OutputData.csv")
+indata = dm.load(ci.outputDataDirectory + ci.inputDataPath)
+outdata = dm.load(ci.outputDataDirectory + ci.outputDataPath)
+
+rf200 = models['RandomForest200']
+rf = ML.generateModel(rf200['model'], rf200['modelArrParameters'],
+                 rf200['modelDictParameters'], indata, outdata)
+dm.save(ci.outputDataDirectory + rf200['savedModelFileName'], rf)
+
+naiveBayes = models['NaiveBayesGaussian']
+nb = ML.generateModel(naiveBayes['model'], naiveBayes['modelArrParameters'],
+                 naiveBayes['modelDictParameters'], indata, outdata)
+                 
+dm.save(ci.outputDataDirectory + naiveBayes['savedModelFileName'], nb)
 
