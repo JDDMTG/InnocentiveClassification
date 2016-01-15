d5764f219c3c0bce59a5cefb53c8b9ef8425e8a1
Juan David <juandominguez2017@u.northwestern.edu>
2016-01-14 21:39:34 -0600

    Adds making model

--- innocentiveMain.py
+++ innocentiveMain.py
@@ -4,20 +4,32 @@ import compInfo as ci
 import machineLearning as ML
 from modelsInfo import models
 #ML.generateTestOutput(dm.load('RF.np'), dm.load(ci.outputDataDirectory + ci.testingInputPathFixed), "OutputData.csv")
-
+2
 def createAndSaveModel(modelDict, indata, outdata, outDict = ''):
     model = ML.generateModel(modelDict['model'], modelDict['modelArrParameters'],
                           modelDict['modelDictParameters'], indata, outdata)
-    dm.save(outDict + model['savedModelFileName'])
+    dm.save(outDict + modelDict['savedModelFileName'], model)
     return model
 
 
 indata = dm.load(ci.outputDataDirectory + ci.inputDataPath)
 outdata = dm.load(ci.outputDataDirectory + ci.outputDataPath)
 
-createAndSaveModel(models['RandomForest200'], indata, outdata, 
-                   ci.outputDataDirectory)
+
+"""
+# createAndSaveModel(models['RandomForest200'], indata, outdata, 
+#                   ci.outputDataDirectory)
                    
-createAndSaveModel(models['NaiveBayesGaussian'], indata, outdata, 
+# createAndSaveModel(models['NaiveBayesGaussian'], indata, outdata, 
+#                   ci.outputDataDirectory)
+
+multilayerPerceptrons = ['MultilayerPerceptronStandard', 'MultilayerPerceptron10050',
+       'MultilayerPerceptron20050', 'MultilayerPerceptron200100']
+       
+for mlp in multilayerPerceptrons:
+    createAndSaveModel(models[mlp], indata, outdata, ci.outputDataDirectory)
+    
+"""
+
+model = createAndSaveModel(models['AdaboostDecisionTree'], indata, outdata, 
                    ci.outputDataDirectory)
-                   
