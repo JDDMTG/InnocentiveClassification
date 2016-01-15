1acb3a73431b4c56c64fe994bd7f49e53f1a744d
MichaelGofron <gofronm@yahoo.com>
2016-01-14 21:37:49 -0600

    (feat) generated test output in main file

--- innocentiveMain.py
+++ innocentiveMain.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import dataMngr as dm
-import compInfo
+import compInfo as ci
 import machineLearning as ML
 
-ML.generateTestOutput(dm.load('RF.np'), dm.load(ci.outputDataDirectory + ci.testingInputPathFixed), "OutputData.csv")
+ML.generateTestOutput(dm.load('adaboost_decision_tree.mod'), dm.load(ci.outputDataDirectory + ci.testingInputPathFixed), "OutputData.csv")
 
